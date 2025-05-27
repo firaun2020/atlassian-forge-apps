3b53004fedae064e515adfc332343e8e65b824e8
Zeeshan Quraishy <zeeshan.quraishy@golar.com>
2025-05-27 14:14:28 +0200

    conditional Ui formatting

--- forge-demo/src/frontend/index.jsx
+++ forge-demo/src/frontend/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import ForgeReconciler, { Text, Em, Strong, Lozenge, Button, Stack } from '@forge/react';
+import ForgeReconciler, { Text, Em, Strong, Lozenge, Button, Stack, Inline } from '@forge/react';
 import { invoke } from '@forge/bridge';
 
 const App = () => {
@@ -7,18 +7,35 @@ const App = () => {
   useEffect(() => {
     invoke('getText', { example: 'my-invoke-variable' }).then(setData);
   }, []);
+
+  // TODO: Make Losenge dynamic
+
+  const looksOk = true;
+  const showRightBanner = (looksOk) ? "success" : "removed";
+  const lorezeTextToShow = (looksOk) ? "PASS" : "FAILED";
+
+
+
+  // TODO: Render Lozenege and text design
+
   return (
     <>
-      <Text><Em>First Text from FE!</Em></Text>
-      <Text><Strong>{data ? data : 'Loading...'}</Strong></Text>
+      <Stack>
+        <Inline alignBlock='baseline' space='space.100'>
+          <Lozenge appearance={showRightBanner}> {lorezeTextToShow} </Lozenge>
+          <Text>This doesn't look right</Text>
+        </Inline>
 
-      <Stack alignInline='start' space='space.200'>
-        <Lozenge appearance='success'>RISK ANAYLYSIS </Lozenge>
-        <Button> THIS BUTTON </Button>
+        <Inline alignBlock='baseline' space='space.100'>
+          <Lozenge appearance='suceess'> YES </Lozenge>
+          <Text>This seems OK</Text>
+        </Inline>
       </Stack>
 
     </>
+
   );
+
 };
 
 ForgeReconciler.render(
@@ -26,6 +43,40 @@ ForgeReconciler.render(
     <App />
   </React.StrictMode>
 );
+
+
+
+
+
+
+// import React, { useEffect, useState } from 'react';
+// import ForgeReconciler, { Text, Em, Strong, Lozenge, Button, Stack } from '@forge/react';
+// import { invoke } from '@forge/bridge';
+
+// const App = () => {
+//   const [data, setData] = useState(null);
+//   useEffect(() => {
+//     invoke('getText', { example: 'my-invoke-variable' }).then(setData);
+//   }, []);
+//   return (
+//     <>
+//       <Text><Em>First Text from FE!</Em></Text>
+//       <Text><Strong>{data ? data : 'Loading...'}</Strong></Text>
+
+//       <Stack alignInline='start' space='space.200'>
+//         <Lozenge appearance='success'>RISK ANAYLYSIS </Lozenge>
+//         <Button> THIS BUTTON </Button>
+//       </Stack>
+
+//     </>
+//   );
+// };
+
+// ForgeReconciler.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 
 
